d270b3a469d05731c751da2d6b38edc2e94af1a6
Sergey Sachkov <43661110+sir-Gee@users.noreply.github.com>
2019-11-10 21:53:02 -0500

    lc101

---
+++ src/TechJobs/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TechJobs.Models;
+
+namespace TechJobs.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+            return View();
+        }
+
+        // TODO #1 - Create a Results action method to process 
+        // search request and display results
+
+
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.jobs = jobs;
+            ViewBag.searchType = searchType;
+            ViewBag.searchTerm = searchTerm;
+            return View("Index");
+        }
+
+        
+
+    
+
+    }
+}
